22bd5ce1bce72e77840063bc07b625bb0e3ebdd7
xczcdjx <10097251+xczcdjx@user.noreply.gitee.com>
2024-12-23 11:44:03 +0800

    判断正误图标处理

--- app/src/main/java/com/xczcdjx/word/components/AnswerText.kt
+++ app/src/main/java/com/xczcdjx/word/components/AnswerText.kt
@@ -2,16 +2,20 @@ package com.xczcdjx.word.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.outlined.Check
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -58,22 +62,39 @@ fun AnswerText(
             else -> AnswerEnum.Default
         }
     }
-    Text(
-        tit,
+    Row(
         modifier
             .clip(RoundedCornerShape(10.dp))
-            .background(when(curAnswer){
-                AnswerEnum.Error->Color.Red
-                AnswerEnum.Right->Color.Green
-                else ->Color.White
-            })
+            .background(
+                when (curAnswer) {
+                    AnswerEnum.Error -> Color.Red
+                    AnswerEnum.Right -> Color.Green
+                    else -> Color.White
+                }
+            )
             .height(50.dp)
             .width(250.dp)
             .clickable {
                 click()
             },
-        textAlign = TextAlign.Center,
-        lineHeight = 50.sp,
-        fontWeight = FontWeight.Bold
-    )
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        if (curAnswer == AnswerEnum.Right) Icon(
+            Icons.Outlined.Check,
+            null,
+            modifier.offset(x = (-20).dp)
+        )
+        else if (curAnswer == AnswerEnum.Error) Icon(
+            Icons.Default.Delete,
+            null,
+            modifier.offset(x = (-20).dp)
+        )
+        Text(
+            tit,
+            textAlign = TextAlign.Center,
+            lineHeight = 50.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
 }
